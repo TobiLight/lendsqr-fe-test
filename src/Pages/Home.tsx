875de1188ec4cf5e36245c8fbb9e1575cb185ef3
Tobilight <jgetitdonefast@gmail.com>
2022-10-17 20:21:47 +0100

    fix: fixed url paths on homepage

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -19,13 +19,13 @@ export default function Home(): JSX.Element {
     return !data ? <p>Loading app...</p> : (<div className="homepage">
         <h1>Hello World</h1>
         <span>Below are the designed pages. Navigate through any one of them</span>
-        <span>* <Link to="login" className="underline-wavy">Login</Link>
+        <span>* <Link to="/login" className="underline-wavy">Login</Link>
         </span>
-        <span>* <Link to="dashboard" className="underline-wavy">Dashboard</Link>
+        <span>* <Link to="/dashboard" className="underline-wavy">Dashboard</Link>
         </span>
-        <span>* <Link to="dashboard/users" className="underline-wavy">Users</Link>
+        <span>* <Link to="/dashboard/users" className="underline-wavy">Users</Link>
         </span>
-        <span>* <Link to="dashboard/user/100029287365456" className="underline-wavy">User Details Page</Link>
+        <span>* <Link to="/dashboard/user/100029287365456" className="underline-wavy">User Details Page</Link>
         </span>
         <p className="">
             This project is developed by Oluwatobiloba 😉
